90a0a0b005f04989b2dd5fa0ae73f3cfb433e150
Michael Forest <unamedpersons@gmail.com>
2020-09-13 20:18:16 +0200

    test 1

--- Steel Engine/Steel Engine/main.cpp
+++ Steel Engine/Steel Engine/main.cpp
@@ -1,10 +1,13 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/window.hpp>
 
+#include <iostream>
+
 #include "SteelEngine.h"
 
 int main()
 {
+    std::cout << "Program turn on" << std::endl;
     SteelEngine engine;
     engine.start();
 
